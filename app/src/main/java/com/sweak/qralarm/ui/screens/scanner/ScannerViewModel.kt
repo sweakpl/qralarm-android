d7473a02a8f27e04520c82a9fad8373ca3e43be3
sweak <hubert.tacik@proton.me>
2024-03-28 23:45:44 +0100

    ScannerViewModel now respects the option of repeating alarms - it reschedules the alarm after it has been dismissed

--- app/src/main/java/com/sweak/qralarm/ui/screens/scanner/ScannerViewModel.kt
+++ app/src/main/java/com/sweak/qralarm/ui/screens/scanner/ScannerViewModel.kt
@@ -9,14 +9,18 @@ import com.google.zxing.Result
 import com.sweak.qralarm.alarm.QRAlarmManager
 import com.sweak.qralarm.data.DataStoreManager
 import com.sweak.qralarm.ui.screens.popBackStackThrottled
+import com.sweak.qralarm.util.ALARM_TYPE_NORMAL
 import com.sweak.qralarm.util.DEFAULT_DISMISS_ALARM_CODE
 import com.sweak.qralarm.util.SCAN_MODE_DISMISS_ALARM
 import com.sweak.qralarm.util.SCAN_MODE_SET_CUSTOM_CODE
 import com.sweak.qralarm.util.Screen
+import com.sweak.qralarm.util.currentTimeInMillis
+import com.sweak.qralarm.util.getAlarmHourOfDay
+import com.sweak.qralarm.util.getAlarmMinute
+import com.sweak.qralarm.util.getAlarmTimeInMillis
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -29,6 +33,7 @@ class ScannerViewModel @Inject constructor(
 ) : ViewModel() {
 
     val scannerUiState = mutableStateOf(ScannerUiState())
+    private var shouldAllowFurtherDecodeResultHandling = true
 
     fun handleDecodeResult(
         result: Result,
@@ -38,7 +43,9 @@ class ScannerViewModel @Inject constructor(
         cancelAlarmSideEffect: () -> Unit
     ) {
         if (scannerMode == SCAN_MODE_DISMISS_ALARM) {
-            if (result.text in getDismissCodes()) {
+            if (result.text in getDismissCodes() && shouldAllowFurtherDecodeResultHandling) {
+                shouldAllowFurtherDecodeResultHandling = false
+
                 val stopAlarmJob = stopAlarm()
 
                 CoroutineScope(Dispatchers.Main).launch {
@@ -66,17 +73,33 @@ class ScannerViewModel @Inject constructor(
         return setOf(userSavedDismissCode, DEFAULT_DISMISS_ALARM_CODE).toList()
     }
 
-    private fun stopAlarm(): Job {
-        val stopAlarmJob = viewModelScope.launch {
-            qrAlarmManager.cancelAlarm()
+    private fun stopAlarm() = viewModelScope.launch {
+        qrAlarmManager.cancelAlarm()
 
+        val isManualAlarmScheduling =
+            dataStoreManager.getBoolean(DataStoreManager.MANUAL_ALARM_SCHEDULING).first()
+        val alarmTime = dataStoreManager.getLong(DataStoreManager.ALARM_TIME_IN_MILLIS).first()
+        val currentTime = currentTimeInMillis()
+        val shouldRepeatAlarm = !isManualAlarmScheduling && alarmTime < currentTime
+
+        if (shouldRepeatAlarm) {
+            val newAlarmTime = getAlarmTimeInMillis(
+                getAlarmHourOfDay(alarmTime),
+                getAlarmMinute(alarmTime)
+            )
+            qrAlarmManager.setAlarm(newAlarmTime, ALARM_TYPE_NORMAL)
+
+            dataStoreManager.apply {
+                putLong(DataStoreManager.ALARM_TIME_IN_MILLIS, newAlarmTime)
+                putBoolean(DataStoreManager.ALARM_SET, true)
+                putBoolean(DataStoreManager.ALARM_SNOOZED, false)
+            }
+        } else {
             dataStoreManager.apply {
                 putBoolean(DataStoreManager.ALARM_SET, false)
                 putBoolean(DataStoreManager.ALARM_SNOOZED, false)
             }
         }
-
-        return stopAlarmJob
     }
 
     private fun setCustomQRCode(code: String) {
