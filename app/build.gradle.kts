1dc70411f0327d0469047fce8b0bf4ea87bdac75
sweak <hubert.tacik@proton.me>
2024-10-16 14:20:32 +0200

    MainActivity onCreate now adjusts alarms

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.code.scanner)
     implementation(libs.androidx.datastore.preferences)
--- app/src/main/java/com/sweak/qralarm/app/MainActivity.kt
+++ app/src/main/java/com/sweak/qralarm/app/MainActivity.kt
@@ -3,10 +3,17 @@ package com.sweak.qralarm.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
+import com.sweak.qralarm.alarm.QRAlarmManager
 import com.sweak.qralarm.core.designsystem.theme.QRAlarmTheme
+import com.sweak.qralarm.core.domain.alarm.AlarmsRepository
 import com.sweak.qralarm.features.add_edit_alarm.navigation.addEditAlarmScreen
 import com.sweak.qralarm.features.add_edit_alarm.navigation.navigateToAddEditAlarm
 import com.sweak.qralarm.features.home.navigation.HOME_SCREEN_ROUTE
@@ -17,11 +24,44 @@ import com.sweak.qralarm.features.introduction.navigation.introductionScreen
 import com.sweak.qralarm.features.scanner.navigation.navigateToScanner
 import com.sweak.qralarm.features.scanner.navigation.scannerScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject lateinit var qrAlarmManager: QRAlarmManager
+
+    @Inject lateinit var alarmsRepository: AlarmsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
+        var areAlarmsAdjusted by mutableStateOf(false)
+        splashScreen.setKeepOnScreenCondition { !areAlarmsAdjusted }
+
+        lifecycleScope.launch {
+            if (!qrAlarmManager.canScheduleExactAlarms()) {
+                alarmsRepository.getAllAlarms().first().forEach { alarm ->
+                    qrAlarmManager.cancelAlarm(alarmId = alarm.alarmId)
+                    alarmsRepository.setAlarmEnabled(
+                        alarmId = alarm.alarmId,
+                        enabled = false
+                    )
+                }
+            } else {
+                alarmsRepository.getAllAlarms().first().forEach { alarm ->
+                    if (alarm.isAlarmEnabled) {
+                        qrAlarmManager.setAlarm(alarmId = alarm.alarmId)
+                    }
+                }
+            }
+
+            areAlarmsAdjusted = true
+        }
 
         setContent {
             QRAlarmTheme {
