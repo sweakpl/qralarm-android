e85f75827bc70f07bf32f9d3d5f06b58da526cbb
sweak <hubert.tacik@proton.me>
2024-10-30 17:38:34 +0100

    Added the empty state to the HomeScreen

--- app/src/main/java/com/sweak/qralarm/features/home/HomeScreen.kt
+++ app/src/main/java/com/sweak/qralarm/features/home/HomeScreen.kt
@@ -5,9 +5,11 @@ import android.net.Uri
 import android.os.Build
 import android.provider.Settings
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
@@ -24,6 +27,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -32,9 +36,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -322,32 +329,73 @@ private fun HomeScreenContent(
                     }
                 }
 
-                items(
-                    items = state.alarmWrappers,
-                    key = { it.alarmId }
-                ) {
-                    AlarmCard(
-                        alarmWrapper = it,
-                        onClick = { alarmId ->
-                            onEvent(HomeScreenUserEvent.EditAlarmClicked(alarmId = alarmId))
-                        },
-                        onAlarmEnabledChanged = { alarmId, enabled ->
-                            onEvent(
-                                HomeScreenUserEvent.AlarmEnabledChangeClicked(
-                                    alarmId = alarmId,
-                                    enabled = enabled
+                if (state.alarmWrappers.isNotEmpty()) {
+                    items(
+                        items = state.alarmWrappers,
+                        key = { it.alarmId }
+                    ) {
+                        AlarmCard(
+                            alarmWrapper = it,
+                            onClick = { alarmId ->
+                                onEvent(HomeScreenUserEvent.EditAlarmClicked(alarmId = alarmId))
+                            },
+                            onAlarmEnabledChanged = { alarmId, enabled ->
+                                onEvent(
+                                    HomeScreenUserEvent.AlarmEnabledChangeClicked(
+                                        alarmId = alarmId,
+                                        enabled = enabled
+                                    )
                                 )
-                            )
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(
-                                start = MaterialTheme.space.medium,
-                                end = MaterialTheme.space.medium,
-                                bottom = MaterialTheme.space.medium
-                            )
-                            .animateItem()
-                    )
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(
+                                    start = MaterialTheme.space.medium,
+                                    end = MaterialTheme.space.medium,
+                                    bottom = MaterialTheme.space.medium
+                                )
+                                .animateItem()
+                        )
+                    }
+                } else {
+                    item {
+                        OutlinedCard(
+                            colors = CardDefaults.outlinedCardColors(
+                                containerColor = Color.Transparent,
+                                contentColor = MaterialTheme.colorScheme.onPrimary
+                            ),
+                            border = BorderStroke(
+                                width = 1.dp,
+                                color = MaterialTheme.colorScheme.onPrimary
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = MaterialTheme.space.medium)
+                        ) {
+                            Column(
+                                verticalArrangement =
+                                Arrangement.spacedBy(MaterialTheme.space.small),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(
+                                        horizontal = MaterialTheme.space.mediumLarge,
+                                        vertical = MaterialTheme.space.medium
+                                    )
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.no_alarms),
+                                    style = MaterialTheme.typography.titleMedium
+                                )
+
+                                Text(
+                                    text = stringResource(R.string.create_using_button),
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+                        }
+                    }
                 }
 
                 item {
