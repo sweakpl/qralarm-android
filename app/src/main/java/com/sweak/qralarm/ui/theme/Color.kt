ad086877e89ecba707d550f7a0a985c3ab7621cc
sweak <hubert.tacik@proton.me>
2024-11-09 11:55:48 +0100

    Removed QRAlarm 1.0 leftovers

--- app/src/main/java/com/sweak/qralarm/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.sweak.qralarm.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Jacarta = Color(0xFF38285C)
-val BlueZodiac = Color(0xFF0C1445)
-val ButterflyBush = Color(0xFF5C54A4)
-val Kimberly = Color(0xFF807CA4)
-val White = Color(0xFFFFFFFF)
