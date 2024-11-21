b14bf040295bf27b978d7875a1c46dde0adb7310
sweak <hubert.tacik@proton.me>
2024-11-21 13:21:18 +0100

    Updated Kotlin to 2.0 + updated CameraX to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.hilt)
     alias(libs.plugins.room)
     alias(libs.plugins.ksp)
