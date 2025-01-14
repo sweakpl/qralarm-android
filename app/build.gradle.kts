0038f7fa900119f4b4e33af8000f0a32388f7b2e
sweak <hubert.tacik@proton.me>
2025-01-14 17:13:28 +0100

    Bumped application version + upgraded AGP to 8.8.0 + updated changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sweak.qralarm"
         minSdk = 21
         targetSdk = 35
-        versionCode = 40
-        versionName = "2.3.1"
+        versionCode = 41
+        versionName = "2.4"
 
         vectorDrawables {
             useSupportLibrary = true
