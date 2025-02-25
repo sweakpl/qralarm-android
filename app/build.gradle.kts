0d3b82342f5f721dcdf47d1ff751aed19b3873d2
sweak <hubert.tacik@proton.me>
2025-02-25 22:30:18 +0100

    Bumped application version + updated AGP and dependencies + updated changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sweak.qralarm"
         minSdk = 21
         targetSdk = 35
-        versionCode = 45
-        versionName = "2.5.1"
+        versionCode = 46
+        versionName = "2.5.2"
 
         vectorDrawables {
             useSupportLibrary = true
