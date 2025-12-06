16ff7aaf0c1174295c54796345aad40809ac370a
sweak <hubert.tacik@proton.me>
2025-12-06 12:01:30 +0100

    Added the alarms chain setting to AddEditAlarmScreen

--- app/src/main/java/com/sweak/qralarm/features/add_edit_alarm/destinations/add_edit/AddEditAlarmScreen.kt
+++ app/src/main/java/com/sweak/qralarm/features/add_edit_alarm/destinations/add_edit/AddEditAlarmScreen.kt
@@ -58,6 +58,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalResources
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -827,6 +828,26 @@ private fun AddEditAlarmScreenContent(
                                                 }
                                             }
                                         }
+
+                                        HorizontalDivider(
+                                            thickness = 1.dp,
+                                            color = MaterialTheme.colorScheme.onSurface,
+                                            modifier = Modifier
+                                                .padding(horizontal = MaterialTheme.space.medium)
+                                        )
+
+                                        SimpleSetting(
+                                            onClick = {
+                                                // TODO
+                                            },
+                                            title = stringResource(R.string.alarms_chain),
+                                            description =
+                                                pluralStringResource(R.plurals.alarms_active, 0, 0),
+                                            icon = QRAlarmIcons.Chain,
+                                            iconContentDescription = stringResource(
+                                                R.string.content_description_chain_icon
+                                            )
+                                        )
 
                                         HorizontalDivider(
                                             thickness = 1.dp,
