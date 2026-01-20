2d871c8be0af10c411d332abe7e4c85f64de3766
sweak <hubert.tacik@proton.me>
2026-01-20 20:29:31 +0100

    Fixed day filter chips colors

--- app/src/main/java/com/sweak/qralarm/features/add_edit_alarm/destinations/add_edit/components/ChooseAlarmRepeatingScheduleBottomSheet.kt
+++ app/src/main/java/com/sweak/qralarm/features/add_edit_alarm/destinations/add_edit/components/ChooseAlarmRepeatingScheduleBottomSheet.kt
@@ -11,10 +11,10 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import com.sweak.qralarm.core.designsystem.component.QRAlarmRadioButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,15 +23,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import com.sweak.qralarm.R
+import com.sweak.qralarm.core.designsystem.component.QRAlarmRadioButton
+import com.sweak.qralarm.core.designsystem.theme.BlueZodiac
 import com.sweak.qralarm.core.designsystem.theme.QRAlarmTheme
+import com.sweak.qralarm.core.designsystem.theme.isQRAlarmTheme
 import com.sweak.qralarm.core.designsystem.theme.space
-import com.sweak.qralarm.core.ui.shortName
 import com.sweak.qralarm.core.ui.model.AlarmRepeatingScheduleWrapper
 import com.sweak.qralarm.core.ui.model.AlarmRepeatingScheduleWrapper.AlarmRepeatingMode
+import com.sweak.qralarm.core.ui.shortName
 import java.time.DayOfWeek
 import java.time.format.TextStyle
 import java.util.Locale
@@ -212,6 +216,24 @@ fun ChooseAlarmRepeatingScheduleBottomSheet(
                                             )
                                         )
                                     },
+                                    border = FilterChipDefaults.filterChipBorder(
+                                        enabled = true,
+                                        selected = dayOfWeek in selectedAlarmDaysOfWeek,
+                                        borderColor = with (MaterialTheme) {
+                                            if (isQRAlarmTheme) Color.White
+                                            else colorScheme.outlineVariant
+                                        }
+                                    ),
+                                    colors = with (MaterialTheme) {
+                                        if (isQRAlarmTheme) {
+                                            FilterChipDefaults.filterChipColors(
+                                                containerColor = Color.Transparent,
+                                                labelColor = Color.White,
+                                                selectedContainerColor = BlueZodiac,
+                                                selectedLabelColor = Color.White
+                                            )
+                                        } else FilterChipDefaults.filterChipColors()
+                                    }
                                 )
                             }
                         }
@@ -228,7 +250,7 @@ private fun ChooseAlarmRepeatingScheduleBottomSheetPreview() {
     QRAlarmTheme {
         ChooseAlarmRepeatingScheduleBottomSheet(
             initialAlarmRepeatingScheduleWrapper = AlarmRepeatingScheduleWrapper(
-                alarmRepeatingMode = AlarmRepeatingMode.ONLY_ONCE,
+                alarmRepeatingMode = AlarmRepeatingMode.CUSTOM,
                 alarmDaysOfWeek = listOf(DayOfWeek.FRIDAY, DayOfWeek.MONDAY)
             ),
             onDismissRequest = {}
