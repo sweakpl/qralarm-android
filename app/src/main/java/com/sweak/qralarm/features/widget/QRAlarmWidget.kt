d8608b692632d6287f46c9abd125d0ff119e1ee6
Sage Johnson <sage.johnson5@gmail.com>
2026-02-22 08:36:57 -0700

    basic template + next alarm time is displaying on widget

--- app/src/main/java/com/sweak/qralarm/features/widget/QRAlarmWidget.kt
+++ app/src/main/java/com/sweak/qralarm/features/widget/QRAlarmWidget.kt
@@ -4,7 +4,23 @@ import android.content.Context
 import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
 import androidx.glance.text.Text
+import com.sweak.qralarm.core.domain.alarm.Alarm
+import com.sweak.qralarm.core.domain.alarm.AlarmsRepository
+import com.sweak.qralarm.core.ui.getTimeString
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.first
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface QRAlarmWidgetEntryPoint {
+    fun alarmsRepository(): AlarmsRepository
+}
 
 class QRAlarmWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
@@ -13,9 +29,46 @@ class QRAlarmWidget : GlanceAppWidget() {
         // Use `withContext` to switch to another thread for long running
         // operations.
 
+        val entryPoint = EntryPointAccessors.fromApplication(
+            context.applicationContext,
+            QRAlarmWidgetEntryPoint::class.java
+        )
+
+        val alarmsRepository = entryPoint.alarmsRepository()
+
+        val allAlarmsFlow = alarmsRepository.getAllAlarms()
+
+        val alarmAsList = allAlarmsFlow.first()
+
+        val nextAlarm = getNextAlarm(alarmAsList)
+
+        val nextAlarmTimeString = getNextAlarmTimeAsString(nextAlarm)
+
         provideContent {
-            // create your AppWidget here
-            Text("Hello World")
+            Column(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Hello World")
+                // Text(anAlarmList[0].alarmLabel.toString())
+
+                // displays the next alarm time!!
+                Text(nextAlarmTimeString)
+            }
+
+
         }
+    }
+
+    fun getNextAlarm(alarmsList: List<Alarm>): Alarm? {
+        val nextAlarm: Alarm? = alarmsList
+            .filter { it.isAlarmEnabled }
+            .minByOrNull { it.nextAlarmTimeInMillis }
+        return nextAlarm
+    }
+
+    // TODO: find out how to see if an alarm is 24HourFormat or not?
+    fun getNextAlarmTimeAsString(alarm: Alarm?): String {
+        return getTimeString(alarm?.nextAlarmTimeInMillis ?: 0, is24HourFormat = false)
     }
 }
